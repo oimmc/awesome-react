26825acd4203b4d77785b9be95e4d402c85a7e98
wangyj <wangyj@ycfin.com.cn>
2020-01-08 18:59:53 +0800

    init

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,8 +6,7 @@ module.exports = {
 	mode: 'development',
 	entry: './src/index.js',
 	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
+		publicPath: '/'
 	},
 	devtool: 'inline-source-map',
 	devServer: {
